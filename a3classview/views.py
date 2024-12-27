9e0a607a7a6bc64d2c26ac6cc671733054a17eaa
jialin <mr.liujalo@gmail.com>
2024-12-27 15:36:30 +0800

    类视图- Mixin 封装APIView

--- a3classview/views.py
+++ a3classview/views.py
@@ -1,37 +1,79 @@
-from rest_framework.views import APIView
+###########################################################################
+#################################APIView###################################
+###########################################################################
+
+# from rest_framework.views import APIView
+# from meituan.models import Merchant
+# from django.http import Http404
+# from rest_framework.response import Response
+# from .serializers import MerchantSerializer
+# from rest_framework import status
+
+
+# class MerchantView(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Merchant.objects.get(pk=pk)
+#         except Merchant.DoesNotExist:
+#             raise Http404
+
+#     def get(self, request, pk=None):
+#         if pk:
+#             merchant = self.get_object(pk)
+#             serializer = MerchantSerializer(merchant)
+#             return Response(serializer.data)
+#         else:
+#             queryset = Merchant.objects.all()
+#             serializer = MerchantSerializer(instance=queryset, many=True)
+#             return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         merchant = self.get_object(pk)
+#         serializer = MerchantSerializer(merchant, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         merchant = self.get_object(pk)
+#         merchant.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+###########################################################################
+#################################Mixin#####################################
+###########################################################################
+
 from meituan.models import Merchant
 from django.http import Http404
 from rest_framework.response import Response
 from .serializers import MerchantSerializer
-from rest_framework import status
+from rest_framework import status, generics, mixins
 
 
-class MerchantView(APIView):
-    def get_object(self, pk):
-        try:
-            return Merchant.objects.get(pk=pk)
-        except Merchant.DoesNotExist:
-            raise Http404
+class MerchantView(
+    generics.GenericAPIView,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+):
+    queryset = Merchant.objects.all()
+    serializer_class = MerchantSerializer
 
     def get(self, request, pk=None):
         if pk:
-            merchant = self.get_object(pk)
-            serializer = MerchantSerializer(merchant)
-            return Response(serializer.data)
-        else:
-            queryset = Merchant.objects.all()
-            serializer = MerchantSerializer(instance=queryset, many=True)
-            return Response(serializer.data)
+            return self.retrieve(request)
+        return self.list(request)
+
+    def post(self, request):
+        # 如果要更改添加逻辑, 重写perform_create方法
+        return self.create(request)
 
     def put(self, request, pk):
-        merchant = self.get_object(pk)
-        serializer = MerchantSerializer(merchant, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.update(request, pk)
 
     def delete(self, request, pk):
-        merchant = self.get_object(pk)
-        merchant.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return self.destroy(request, pk)
