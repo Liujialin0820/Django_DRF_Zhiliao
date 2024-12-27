3608b3e641b64675671ff346dbae4ceccf573e93
jialin <mr.liujalo@gmail.com>
2024-12-27 16:11:18 +0800

    类视图- ApiView 封装 Mixin

--- a3classview/urls.py
+++ a3classview/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import MerchantView
+from .views import MerchantView,MerchantDetailView
 
 app_name = "classview"
 urlpatterns = [
     path("merchant/", MerchantView.as_view()),
-    path("merchant/<int:pk>/", MerchantView.as_view()),
+    path("merchant/<int:pk>/", MerchantDetailView.as_view()),
 ]
--- a3classview/views.py
+++ a3classview/views.py
@@ -45,35 +45,63 @@
 #################################Mixin#####################################
 ###########################################################################
 
+# from meituan.models import Merchant
+# from django.http import Http404
+# from rest_framework.response import Response
+# from .serializers import MerchantSerializer
+# from rest_framework import status, generics, mixins
+
+
+# class MerchantView(
+#     generics.GenericAPIView,
+#     mixins.ListModelMixin,
+#     mixins.RetrieveModelMixin,
+#     mixins.CreateModelMixin,
+#     mixins.UpdateModelMixin,
+#     mixins.DestroyModelMixin,
+# ):
+#     queryset = Merchant.objects.all()
+#     serializer_class = MerchantSerializer
+
+#     def get(self, request, pk=None):
+#         if pk:
+#             return self.retrieve(request)
+#         return self.list(request)
+
+#     def post(self, request):
+#         # 如果要更改添加逻辑, 重写perform_create方法
+#         return self.create(request)
+
+#     def put(self, request, pk):
+#         return self.update(request, pk)
+
+#     def delete(self, request, pk):
+#         return self.destroy(request, pk)
+
+###########################################################################
+#################################APIView###################################
+###########################################################################
+
 from meituan.models import Merchant
-from django.http import Http404
-from rest_framework.response import Response
 from .serializers import MerchantSerializer
-from rest_framework import status, generics, mixins
+from rest_framework import generics
 
 
 class MerchantView(
-    generics.GenericAPIView,
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.CreateModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
+    generics.ListAPIView,
+    generics.CreateAPIView,
 ):
     queryset = Merchant.objects.all()
     serializer_class = MerchantSerializer
 
-    def get(self, request, pk=None):
-        if pk:
-            return self.retrieve(request)
-        return self.list(request)
 
-    def post(self, request):
-        # 如果要更改添加逻辑, 重写perform_create方法
-        return self.create(request)
-
-    def put(self, request, pk):
-        return self.update(request, pk)
-
-    def delete(self, request, pk):
-        return self.destroy(request, pk)
+class MerchantDetailView(
+    generics.DestroyAPIView,
+    generics.UpdateAPIView,
+    generics.RetrieveAPIView,
+):
+    queryset = Merchant.objects.all()
+    serializer_class = MerchantSerializer
+    # lookup_field='name'
+    # URL处要一并修改
+    # path("merchant/<str:name>/", MerchantDetailView.as_view()),
