acf1d9481b54faefe52eddf3d1eec06211fa2bb7
jialin <mr.liujalo@gmail.com>
2024-12-28 19:05:26 +0800

    异常处理 - 自定义异常处理2

--- a6exception/views.py
+++ a6exception/views.py
@@ -1,8 +1,14 @@
 from meituan.models import Merchant
 from .serializers import MerchantSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
 
 class MerchantViewSet(viewsets.ModelViewSet):
     queryset = Merchant.objects.all()
     serializer_class = MerchantSerializer
+
+    @action(["GET"], detail=False)
+    def my(self, request):
+        Merchant.objects.update(id=1)
+        return {}
